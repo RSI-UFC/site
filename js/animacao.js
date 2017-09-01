7f642a80f412d135d7d882b3fc2a7cb15cede6b6
RSI - Residência em Segurança da Informação <rsi@dc.ufc.br>
2017-09-01 17:55:15 -0300

    Primeira versão publica do site principal.

---
+++ js/animacao.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.nav-link, #btn-inicio, .footer-link').click(function(e){
+    $('.collapse').collapse('hide');
+    var linkHref = $(this).attr("href");
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    }, 700);
+    e.preventDefault();
+  });
+});
